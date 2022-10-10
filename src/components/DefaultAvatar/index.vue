c1b5918e9c325883313e48adba6370b47d06b978
shisihao <1023559582@qq.com>
2022-10-10 15:28:49 +0800

    style: 头像、我的认证

--- src/components/DefaultAvatar/index.vue
+++ src/components/DefaultAvatar/index.vue
@@ -1,6 +1,5 @@
 <template>
   <van-image
-    class="default-avatar"
     fit="cover"
     :round="round"
     :src="src"
@@ -8,7 +7,6 @@
 </template>
 
 <script setup>
-import { defineProps } from 'vue'
 import { getImageUrl } from '@/utils/index'
 
 const props = defineProps({
@@ -18,38 +16,11 @@ const props = defineProps({
   },
   src: {
     type: String,
-    default: getImageUrl('public/logo.png')
+    default: ''
   },
 })
 
 </script>
 
 <style lang="scss" scoped>
-.default-avatar {
-  padding: 1px;
-  position: relative;
-  :deep(img) {
-    position: relative;
-    z-index: 1;
-  }
-  &::before {
-    content: '';
-    width: 100%;
-    height: 100%;
-    position: absolute;
-    left: 0;
-    top: 0;
-    background-image: var(--root-button-color1);
-  }
-  &::after {
-    content: '';
-    width: calc(100% - 2px);
-    height: calc(100% - 2px);
-    position: absolute;
-    left: 1px;
-    top: 1px;
-    background-color: #fff;
-    border-radius: 50%;
-  }
-}
 </style>
