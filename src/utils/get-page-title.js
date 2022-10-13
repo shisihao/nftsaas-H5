6d8f74b85cecb267da8c103c66dd5fd87f8c43ed
ThirteenC <192537723@qq.com>
2022-10-13 11:21:25 +0800

    improvement: title完善

--- src/utils/get-page-title.js
+++ src/utils/get-page-title.js
@@ -1,10 +1,9 @@
-import defaultSettings from '@/settings'
 import store from '@/store/index'
 
 export default function getPageTitle( pageTitle ) {
   const title = store.state.settings.title || ''
   if ( pageTitle ) {
-    return `${pageTitle} - ${title}`
+    return `${pageTitle} ${title ? '-' : ''} ${title}`
   }
   return `${title}`
 }
